67c1204aba58db700731d23d4e0b310fcaa9b500
Lionel Blatter <lionel.blatter@kit.edu>
2021-05-09 23:05:34 +0200

    finalized the example insertion_sort

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -23,22 +23,10 @@
         same_elements{L1, L2}(a, b, begin, end) ==>
         same_elements{L2, L3}(b, c, begin, end) ==>
         same_elements{L1, L3}(a, c, begin, end);
-      case split {L1, L2}: \forall int *a, int *b, integer begin, i, end;
-        begin <= i < end ==>
-        same_elements{L1,L2}(a, b, begin, i) ==>
-        same_elements{L1,L2}(a, b, i, end) ==>
-        same_elements{L1,L2}(a, b, begin, end);
 }*/
 
-/*@ lemma partition {L1, L2}: \forall int *a, int *b, integer begin, i, j, end;
-        begin <= i <= j < end ==>
-        same_array{L1,L2}(a, b, begin, i) ==>
-        same_elements{L1,L2}(a, b, i, j) ==>
-        same_array{L1,L2}(a, b, j, end) ==>
-        same_elements{L1, L2}(a, b, begin, end);
-*/
-
 /*@ requires 0 <= n;
+    requires \valid(tab+(0..n-1));
     requires sorted(tab,n);
     ensures \forall int j; 0 <= j < \result ==> tab[j] <= val;
     ensures \forall int j; \result <= j < n ==> tab[j] > val;
@@ -60,26 +48,28 @@ int chercher_limite(int tab[], int n, int val){
   return n;
 }
 
-/*@ requires 0 <= idx <= n;
-    requires \separated(tab+(0..n),gtab+(0..n));
+/*@ requires 0 <= idx <= n < end;
+    requires \valid(tab+(idx..n));
+    requires \valid(gtab+(idx..n));
+    requires \separated(tab+(0..n),gtab+(0..end-1));
     requires same_array{Pre, Pre}(tab, gtab, 0, n);
     ensures \forall int j; idx < j <= n ==> tab[j] == \at(tab[j-1],Pre);
     ensures \forall int j; 0 <= j <= idx ==> tab[j] == \at(tab[j],Pre);
-    ensures same_elements{Pre, Post}(gtab, gtab, 0, n+1);
+    ensures same_elements{Pre, Post}(gtab, gtab, 0, end);
     ensures \forall integer k; 0 <= k <= n && k != idx ==> tab[k] == gtab[k];
     ensures \at(gtab[idx],Post) == \at(gtab[n],Pre);
-    assigns tab[0..n],gtab[0..n];
+    assigns tab[idx..n],gtab[idx..n];
 */
-void decale(int tab[], int n, int idx) /*@ ghost (int gtab[])*/{
+void decale(int tab[], int n, int idx) /*@ ghost (int gtab[], int end)*/{
   int i;
   /*@ loop invariant idx <= i <= n;
       loop invariant \forall int j; i < j <= n ==> tab[j] == \at(tab[j-1],Pre);
       loop invariant \forall int j; 0 <= j <= i ==> tab[j] == \at(tab[j],Pre);
-      loop invariant same_elements{Pre, Here}(gtab, gtab, 0, n+1);
+      loop invariant same_elements{Pre, Here}(gtab, gtab, 0, end);
       loop invariant \forall integer k; i < k <= n ==> tab[k] == gtab[k];
       loop invariant \forall integer k; 0 <= k < i  ==> tab[k] == gtab[k];
       loop invariant \at(gtab[i],Here) == \at(gtab[n],Pre);
-      loop assigns i, tab[0..n], gtab[0..n];
+      loop assigns i, tab[idx..n],gtab[idx..n];
       loop variant i;
    */
   for (i = n; i > idx; i--){
@@ -88,39 +78,38 @@ void decale(int tab[], int n, int idx) /*@ ghost (int gtab[])*/{
     /*@ ghost int temp = gtab[i];*/
     /*@ ghost gtab[i] = gtab[i-1];*/
     /*@ ghost gtab[i-1] = temp;*/
-    /*@ assert swap{L1, Here}(gtab, gtab, 0, i-1, i, n+1);*/
+    /*@ assert swap{L1, Here}(gtab, gtab, 0, i-1, i, end);*/
   }
   return;
 }
 
-/*@ ghost int *t;*/ //use a local array and seach how to handle the case n = 0
+/*@ ghost int\ghost* t;*/
 
 /*@ requires 0 <= n;
-  requires \separated(tab+(0..n-1),t+(0..n-1));
-  requires same_array{Pre, Pre}(tab, t, 0, n);
-  ensures sorted(tab,n);
-  ensures same_elements{Pre, Post}(tab,tab, 0, n );
-  assigns tab[0..n-1],t[0..n-1];*/
+    requires \separated(tab+(0..n-1),t+(0..n-1));
+    requires \valid(tab+(0..n-1));
+    requires \valid(t+(0..n-1));
+    requires same_array{Pre, Pre}(tab, t, 0, n);
+    ensures sorted(tab,n);
+    ensures same_elements{Pre, Post}(tab,tab, 0, n );
+    assigns tab[0..n-1],t[0..n-1];*/
 void tri_insertion(int tab[], int n){
   int lim;
   /*@ loop invariant 0 <= lim <= n;
-      loop invariant sorted(tab,lim);
       loop invariant \forall int j; lim <= j < n ==> tab[j] == \at(tab[j],Pre);
       loop invariant same_array{Here, Here}(tab, t, 0, n);
+      loop invariant sorted(tab,lim);
       loop invariant same_elements{Pre, Here}(t,t, 0, n );
-      loop assigns tab[0..n-1],t[0..n-1],lim;
+      loop assigns tab[0..n-1],lim,t[0..n-1];
       loop variant n - lim;
    */
   for (lim = 0; lim < n; lim++){
   l1:;
     int val = tab[lim];
     int idx = chercher_limite(tab,lim,val);
-    decale(tab,lim,idx)/*@ ghost (t)*/;
-    /*@ assert same_elements{l1,Here}(t,t,0,n);*/
-    /*@ assert same_elements{Pre, Here}(t,t, 0, n );*/
+    decale(tab,lim,idx)/*@ ghost (t,n)*/;
     tab[idx] = val;
-    /*@ assert sorted(tab,lim);*/
-    /*@ assert same_elements{Pre, Here}(t,t, 0, n );*/
+    /*@ assert same_elements{l1,Here}(t,t,0,n);*/
   }
   return;
 }
